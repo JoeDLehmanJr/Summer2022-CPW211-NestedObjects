e3e42ac5c6c9d496d87792e99944860533f6abe4
JoeLehmanJr <joejr65@live.com>
2022-07-15 19:07:50 -0700

    Added test code to test the new classes.

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NestedObjects;
+
+Advisor compSciAdvisor = new()
+{
+    FullName = "Annemarie Smith",
+    Email = "Annemarie@cptc.edu",
+    OfficeLocation = "B17 Rm 150",
+};
+
+Student stu1 = new()
+{
+    FirstName = "Homer",
+    LastName = "Simpson",
+    DateOfBirth = new DateOnly(2000, 3, 20),
+    AssignedAdvisor = compSciAdvisor,
+    PhoneNumber = "2535899999",
+    SchoolEmail = "Homer.Simpson@students,cptc.edu"
+};
+
+Console.WriteLine($"Student: {stu1.FirstName} has {stu1.AssignedAdvisor.FullName}");
